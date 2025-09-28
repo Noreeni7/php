e6afd6eeac73b09554b4cd85df88a5fe629c567e
Noreen Chepkurui <noreenkoskeii7@gmail.com>
2025-09-28 21:04:16 +0300

    Add redirect to index.php

--- includes/formhandler.php
+++ includes/formhandler.php
@@ -2,12 +2,15 @@
 
 // var_dump($_SERVER["REQUEST_METHOD"]);
 
+// Check if the form was submitted using the POST method
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // Get submitted form data and sanitize it to prevent XSS
     $first = htmlspecialchars($_POST["firstname"]);
     $second = htmlspecialchars($_POST["secondname"]);
     $pet = htmlspecialchars($_POST["favouritepet"]);
 
-
+    // Display submitted data back to the user
     echo "This is the data that the user subitted:";
     echo "<br>";
     echo $first;
@@ -16,4 +19,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     echo "<br>";
     echo $pet;
     
+    // Redirect the user back to the homepage (index.php)
+    // In both cases (if condition is met or not), the user is sent to index.php
+    header("Location: ../index.php");
+}
+else{
+    header("Location: ../index.php");
 }
