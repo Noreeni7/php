db5d4cbfc6a9c536807d62ae24442ae5f7b650a5
Noreen Chepkurui <noreenkoskeii7@gmail.com>
2025-09-28 16:24:58 +0300

    Data types and variables

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    //  ================== VARIABLES ============ 
+    $name = "David";
+    echo $name;
+
+    //  ================== DATA TYPES ============ 
+    // Scalar types (contains one value)
+    $string ="David";
+    $int = 123987;
+    $float = 2.8985;
+    $bool = false;
+
+    // Array type
+    $names =["Faith", "Asha", "Frida"];
+
+    // Oject types
+
+    ?>
+
+    <p>Hi! My name is <?php echo $string; ?>, and I am a teacher</p>
+
+    
+
+
+
+
+
+</body>
+</html>
