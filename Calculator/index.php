a5259a87f463d1a7c59bd918cf9d004bd655c3dd
Noreen Chepkurui <noreenkoskeii7@gmail.com>
2025-09-30 13:07:52 +0300

    Add calculator error handing and operation logic

--- Calculator/index.php
+++ Calculator/index.php
@@ -17,16 +17,51 @@
             <option value="divide">/</option>
             <option value="multiply">*</option>
         </select>
-        <input type="number" name="num02" placeholder="First number">
-        <button>Calculate</button>
+        <input type="" name="num02" placeholder="Second number">
+        <button type="submit">Calculate</button>
     </form>
 
     <?php 
         if($_SERVER["REQUEST_METHOD"] == "POST"){
             
-            // grab data from inputs
+            //Grab data from inputs
             $firstnumber = filter_input(INPUT_POST, "num01", FILTER_SANITIZE_NUMBER_FLOAT);
             $secondnumber = filter_input (INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT);
+            $operator = htmlspecialchars($_POST["operator"]);
+
+            // Error handlers
+            $errors = false;
+
+            if(empty($firstnumber) || empty($secondnumber) || empty($operator)){
+                echo "<p class='calc-error'> Fill in all fields!</p>";
+                $errors = true;
+            }
+
+            if(!is_numeric($firstnumber) || !is_numeric($secondnumber)){
+                echo "<p class='calc-error'> Only type numbers!</p>";
+                $errors = true;
+            }
+
+            if(!$errors){
+                $value = 0;
+                switch ($operator){
+                    case "add":
+                        $value = $firstnumber + $secondnumber;
+                        break;
+                    case "subtract":
+                        $value = $firstnumber - $secondnumber;
+                        break;
+                    case "divide":
+                        $value = $firstnumber / $secondnumber;
+                        break;
+                    case "multiply":
+                        $value = $firstnumber * $secondnumber; 
+                        break;
+                    default:
+                        echo "<p class='calc-error'> Something went HORRIBLY wrong!</p>";
+                }
+                echo "<p class='calc-error'> Result = $value </p>";
+            }
         }
     ?>
 
