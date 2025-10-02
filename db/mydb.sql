0f1c7fd5761f6bd6710fd8b7e5dbb7637705cebf
Noreen Chepkurui <noreenkoskeii7@gmail.com>
2025-10-02 20:08:59 +0300

    Change table data

---
+++ db/mydb.sql
@@ -0,0 +1,36 @@
+CREATE TABLE users(
+	id INT(11) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(30) NOT NULL,
+    pwd VARCHAR(255) NOT NULL,
+    email VARCHAR (100) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT TIME,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE comments(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(30) NOT NULL,
+    comment_text TEXT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIME,
+    users_id INT(11),
+    PRIMARY KEY (id),
+    FOREIGN KEY (users_id) REFERENCES users (id) ON DELETE SET NULL
+);
+
+
+Options:
+
+ON DELETE CASCADE → if the user is deleted, delete all their posts too.
+
+ON DELETE RESTRICT → do not allow deleting the user if they still have posts.
+
+ON DELETE SET NULL → keep the post, but mark user_id as NULL.
+
+
+INSERT INTO users (username, pwd, email) VALUES ('Fredrick', 'fred123', 'fred12@gmail.com');
+
+UPDATE users SET username = 'Kip', pwd = 'kip' WHERE id = 2;
+
+DELETE FROM users WHERE id = 1;
+
+INSERT INTO comments (username, comment_text, users_id) VALUES ('Kip', 'This is a comment' , 2);
