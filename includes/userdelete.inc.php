b5c2777a9b93c9fd5de0327ca75c41923a179632
Noreen Chepkurui <noreenkoskeii7@gmail.com>
2025-10-07 06:38:54 +0300

    Correct POST check and variable references in user dee\te script

--- includes/userdelete.inc.php
+++ includes/userdelete.inc.php
@@ -1,7 +1,7 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] = "POST") {
-    $username = $_POST["$username"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
     $pwd = $_POST["pwd"];
 
     try {
